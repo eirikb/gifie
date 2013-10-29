8fc5f440f9846cdf95ccd5184024b75bf0ebff39
eirikb <eirikb@eirikb.no>
2013-10-29 23:11:26 +0100

    Convert to jQuery for no good reason

--- gifie.js
+++ gifie.js
@@ -1,14 +1,11 @@
-window.onload = function() {
-  var video = document.createElement('video');
-  var canvas = document.createElement('canvas');
-  var img = document.querySelector('img');
-  var record = document.querySelector('button');
-  var label = document.querySelector('label');
+$(function() {
+  var video = $('<video autoplay>')[0];
+  var canvas = $('<canvas>')[0];
   var ctx = canvas.getContext('2d');
   var worker = new Worker('worker.js');
   var width = 320;
   var height = 240;
-  var maxFrames = 20;
+  var maxFrames = 15;
   var delay = 200;
   var frames;
 
@@ -17,9 +14,10 @@ window.onload = function() {
   video.height = canvas.height = height;
 
   worker.addEventListener('message', function(event) {
-    img.src = 'data:image/gif;base64,' + btoa(event.data);
-    img.classList.remove('hide');
-    label.className = 'hide';
+    var dataUrl = 'data:image/gif;base64,' + btoa(event.data);
+    $('img').attr('src', dataUrl).removeClass('hide');
+    $('#imglink').attr('href', dataUrl);
+    $('label').addClass('hide');
   }, false);
 
   function snapshot() {
@@ -28,14 +26,15 @@ window.onload = function() {
       ctx.drawImage(video, 0, 0, width, height);
       worker.postMessage(ctx.getImageData(0, 0, width, height).data);
     } else if (frames === 0) {
-      record.classList.remove('disabled');
-      record.classList.remove('alert-danger');
-      label.className = '';
+      $('button').removeClass('disabled');
+      $('.record').text('Record');
+      $('label').removeClass('hide');
       worker.postMessage({
         cmd: 'end'
       });
     }
   }
+
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
@@ -43,12 +42,13 @@ window.onload = function() {
     video: true
   }, function(stream) {
     video.src = window.URL.createObjectURL(stream);
-    record.classList.remove('hide');
+    $('button').removeClass('hide');
   }, function(err) {
+    $('#error').removeClass('hide');
     console.log(err);
   });
 
-  record.addEventListener('click', function() {
+  $('button').click(function() {
     worker.postMessage({
       cmd: 'init',
       width: width,
@@ -56,11 +56,11 @@ window.onload = function() {
       delay: delay
     });
 
-    img.classList.add('hide');
-    record.classList.add('disabled');
-    record.classList.add('alert-danger');
+    $('img').addClass('hide');
+    $(this).addClass('disabled');
+    $('.record').text('Recording...');
     frames = maxFrames;
   });
 
   setInterval(snapshot, delay);
-};
+});
